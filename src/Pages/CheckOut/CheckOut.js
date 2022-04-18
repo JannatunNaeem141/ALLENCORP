106b052595e86e5c5ae982464895883c02c03fea
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-04-18 23:10:11 +0600

    README.md file updated

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -2,44 +2,12 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import './CheckOut.css'
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const CheckOut = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = async (data) => {
-        await sleep(2000);
-        if (data.username === "bill") {
-            alert(JSON.stringify(data));
-        } else {
-            alert("There is an error");
-        }
-    };
-    console.log(errors);
 
     return (
-        // <div className='checkout'>
-        //     <h2 className='checkout-text'>Checkout content is coming soon...</h2>
-        // </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <h1>CheckOut</h1>
-            <label htmlFor="firstName">First Name</label>
-            <input placeholder="First Name" {...register("firstName")} />
-
-            <label htmlFor="lastName">Last Name</label>
-            <input placeholder="Last Name" {...register("lastName")} />
-
-            <label htmlFor="email">Email</label>
-            <input
-                placeholder="Your Email"
-                type="text"
-                {...register("email")}
-            />
-
-            <div style={{ color: "red" }}>
-                {Object.keys(errors).length > 0 &&
-                    "There are errors, check your console."}
-            </div>
-            <input type="submit" className='checkout-btn' />
-        </form>
+        <div className='checkout'>
+            <h2 className='checkout-text'>Checkout content is coming soon...</h2>
+        </div>
     );
 };
 
